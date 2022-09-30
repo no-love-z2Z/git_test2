e54720c1803dc274846a08cc75da8764ad94cee8
“qingquan” <hello@itcast.cn>
2022-09-30 20:05:26 +0800

    hot-fix commit

--- src/main/java/com/itheima/Test1.java
+++ src/main/java/com/itheima/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("commit 1");
         System.out.println("create new branches");
+        System.out.println("hot-fix");
     }
 }
