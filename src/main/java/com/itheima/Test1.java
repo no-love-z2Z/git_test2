3936e2ec201fab1bd5b965c24e721a60f5eb4f46
“qingquan” <hello@itcast.cn>
2022-09-30 20:04:30 +0800

    create hot-fix

--- src/main/java/com/itheima/Test1.java
+++ src/main/java/com/itheima/Test1.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("commit 1");
+        System.out.println("create new branches");
     }
 }
